a4461782b73056daab0b03f979b90884d5ec8649
SavvyProp <ludwigtaycheeying@gmail.com>
2025-09-06 15:57:29 -0400

    lower sim time

--- playground/booster/config.py
+++ playground/booster/config.py
@@ -14,5 +14,5 @@ ppo_params.network_factory = config_dict.create(
         noise_std_type = "scalar"
     )
 
-ppo_params.num_timesteps = 300_000_000
+ppo_params.num_timesteps = 200_000_000
 #ppo_params.normalize_observations = False
