2a23923625044ef41538734414ddd92932bf7ddf
SavvyProp <ludwigtaycheeying@gmail.com>
2025-09-25 20:09:21 -0400

    lowered qc gain

--- lowctrl/maqp.py
+++ lowctrl/maqp.py
@@ -367,8 +367,8 @@ def highlvlPD(data, des_pos, des_com_vel, des_angvel, ids):
     qpos = data.qpos[ids["joint_pos_ids"]]
     qvel = data.qvel[ids["joint_vel_ids"]]
 
-    jp_gain = 800.0
-    jd_gain = 40.0
+    jp_gain = 400.0
+    jd_gain = 20.0
 
     world_com_vel = lmath.rotate_des_com_vel(des_com_vel, data)
 
