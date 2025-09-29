4d8f12b64ed1dcba36e7eb1a8c066e345c54eab3
SavvyProp <ludwigtaycheeying@gmail.com>
2025-09-29 09:47:08 -0400

    bigger network

--- playground/booster/config.py
+++ playground/booster/config.py
@@ -1,7 +1,7 @@
 from ml_collections import config_dict
 
 ppo_params = config_dict.create(
-      num_timesteps=200_000_000,
+      num_timesteps=100_000_000,
       num_evals=10,
       reward_scaling=1.0,
       episode_length=500,
@@ -17,26 +17,26 @@ ppo_params = config_dict.create(
       batch_size=256,
       clipping_epsilon=0.2,
       max_grad_norm=1.0,
-      network_factory=config_dict.create(
-          policy_hidden_layer_sizes=(512, 256, 256, 128),
-          value_hidden_layer_sizes=(512, 256, 256, 128),
-          policy_obs_key="state",
-          value_obs_key="privileged_state",
-          #distribution_type = "tanh_normal",
-          #noise_std_type = "log"
-      ),
       num_resets_per_eval=1,
   )
 
-
 ppo_params.network_factory = config_dict.create(
-        policy_hidden_layer_sizes=(512, 256, 256, 128),
-        value_hidden_layer_sizes=(512, 256, 256, 128),
+        policy_hidden_layer_sizes=(1024, 512, 512, 256, 256),
+        value_hidden_layer_sizes=(1024, 512, 512, 256, 256),
         policy_obs_key="state",
         value_obs_key="privileged_state",
         distribution_type = "normal",
         noise_std_type = "scalar"
     )
 
-ppo_params.num_timesteps = 200_000_000
+#ppo_params.network_factory = config_dict.create(
+#        policy_hidden_layer_sizes=(512, 256, 256, 128),
+#        value_hidden_layer_sizes=(512, 256, 256, 128),
+#        policy_obs_key="state",
+#        value_obs_key="privileged_state",
+#        distribution_type = "normal",
+#        noise_std_type = "scalar"
+#    )
+
+ppo_params.num_timesteps = 100_000_000
 #ppo_params.normalize_observations = False
