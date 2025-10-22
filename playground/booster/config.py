8dd473fa2a4311e9d68c1e8cc7764e487a0b7454
SavvyProp <ludwigtaycheeying@gmail.com>
2025-10-22 17:57:58 -0400

    motion_retarget

--- playground/booster/config.py
+++ playground/booster/config.py
@@ -72,5 +72,5 @@ ppo_params.network_factory = config_dict.create(
 #        noise_std_type = "scalar"
 #    )
 
-ppo_params.num_timesteps = 120_000_000
+ppo_params.num_timesteps = 40_000_000
 #ppo_params.normalize_observations = False
