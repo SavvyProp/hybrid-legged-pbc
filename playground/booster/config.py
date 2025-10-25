84e1c8c20be6adab63b9c424a2d2c8d326fe9941
SavvyProp <ludwigtaycheeying@gmail.com>
2025-10-25 18:37:38 -0400

    traj center

--- playground/booster/config.py
+++ playground/booster/config.py
@@ -72,5 +72,5 @@ ppo_params.network_factory = config_dict.create(
 #        noise_std_type = "scalar"
 #    )
 
-ppo_params.num_timesteps = 100_000_000
+ppo_params.num_timesteps = 60_000_000
 #ppo_params.normalize_observations = False
