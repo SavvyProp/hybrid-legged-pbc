3a8ce9132bd99fd4f6afa8e3d6aa3907b6c976a0
SavvyProp <ludwigtaycheeying@gmail.com>
2025-10-27 14:09:44 -0400

    pain

--- playground/booster/track.py
+++ playground/booster/track.py
@@ -51,7 +51,7 @@ def step(
   return jax.lax.scan(single_step, data, (), n_substeps)[0]
 
 ppo_params = config_dict.create(
-      num_timesteps=5_000_000,
+      num_timesteps=50_000_000,
       num_evals=10,
       reward_scaling=1.0,
       episode_length=770,
